4500bbe50ce2e4fd7dadb5da19105871bd6ed11d
khindaraj <111604705+khindaraj@users.noreply.github.com>
2024-06-14 00:06:29 -0700

    Update create_traffic_services.py

--- create_traffic_services.py
+++ create_traffic_services.py
@@ -10,7 +10,7 @@ domain_files = [
     os.path.join(script_directory, "top-10k.txt")
 ]
 
-def create_traffic_generator_service(filename, gateway):
+def create_traffic_generator_service(filename, filename_full, gateway):
     gateway_argument = f"--gateway {gateway}"
 
     service_content = f"""
@@ -21,24 +21,32 @@ After=network-online.target
 
 [Service]
 Type=simple
-#User=lab-user  # Replace with your username
+User=lab-user  # Replace with your username
 WorkingDirectory={script_directory}
-ExecStart=/usr/bin/python3 gp-traffic-gen.py --domains {filename} --insecure {gateway_argument}
+ExecStart=/usr/bin/python3 gp-traffic-gen.py --domains {filename_full} --insecure {gateway_argument}
 
 [Install]
 WantedBy=multi-user.target
 """
+
     service_filename = f"{filename}.service"
     with open(os.path.join("/etc/systemd/system", service_filename), "w") as f:
         f.write(service_content)
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: script.py <gateway>")
+        sys.exit(1)
+
     gateway = sys.argv[1]
     for domain_file in domain_files:
-        create_traffic_generator_service(domain_file, gateway)
+        filename = os.path.splitext(os.path.basename(domain_file))[0]
+        filename_full = f"{filename}.txt"  # Add the .txt extension here
+        create_traffic_generator_service(filename, filename_full, gateway)
 
     subprocess.run(["sudo", "systemctl", "daemon-reload"])
     for domain_file in domain_files:
         filename = os.path.splitext(os.path.basename(domain_file))[0]
         subprocess.run(["sudo", "systemctl", "enable", f"{filename}.service"])
         subprocess.run(["sudo", "systemctl", "start", f"{filename}.service"])
+
